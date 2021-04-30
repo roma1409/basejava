e0e20ffc7a6fdb2e68aab22802892d492a30f73b
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-04-30 16:03:34 +0300

    2. Moved to the proper packages & added update method.

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -1,3 +1,8 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,7 +18,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | update uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -32,7 +37,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
@@ -45,6 +50,12 @@ public class MainArray {
                     break;
                 case "clear":
                     ARRAY_STORAGE.clear();
+                    printAll();
+                    break;
+                case "update":
+                    r = new Resume();
+                    r.setUuid(uuid);
+                    ARRAY_STORAGE.update(r);
                     printAll();
                     break;
                 case "exit":
---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,91 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private final Resume[] storage = new Resume[10_000];
+    private int size = 0;
+
+    public void update(Resume resume) {
+        int index = findIndex(resume.getUuid());
+        if (index == -1) {
+            showAbsenceResumeError("UPDATE", resume.getUuid());
+        } else {
+            storage[index] = resume;
+        }
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume resume) {
+        boolean isResumeNew = findIndex(resume.getUuid()) == -1;
+        if (!isResumeNew) {
+            System.out.printf("You can't 'SAVE' resume due to there is already a resume with this uuid: '%s'.%n", resume.getUuid());
+        }
+        boolean isEnoughSpaceInStorage = size < storage.length;
+        if (!isEnoughSpaceInStorage) {
+            System.out.println("You can't 'SAVE' resume due to there isn't enough space to add any new resume.");
+        }
+
+        if (isResumeNew && isEnoughSpaceInStorage) {
+            storage[size++] = resume;
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = findIndex(uuid);
+        if (index == -1) {
+            showAbsenceResumeError("GET", uuid);
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index == -1) {
+            showAbsenceResumeError("DELETE", uuid);
+        } else {
+            shiftArray(index);
+        }
+    }
+
+    private void showAbsenceResumeError(String operation, String uuid) {
+        System.out.printf("You can't '%s' resume due to there isn't resume with this uuid: '%s'.%n", operation, uuid);
+    }
+
+    private int findIndex(String uuid) {
+        return IntStream.range(0, size)
+                .filter(i -> uuid.equals(storage[i].getUuid()))
+                .findFirst()
+                .orElse(-1);
+    }
+
+    private void shiftArray(int start) {
+        int length = size - 1 - start;
+        if (length > 0) {
+            System.arraycopy(storage, start + 1, storage, start, length);
+        }
+        storage[--size] = null;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+}
