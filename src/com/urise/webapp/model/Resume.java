0ee8bd39385a2d9dca4cd3e3fdc467023ecb9bd6
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-05-03 07:33:21 +0300

    4.2. Added 'constructor' usage.

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -6,14 +6,14 @@ import java.util.Objects;
  * Initial resume class
  */
 public class Resume implements Comparable<Resume> {
-    private String uuid;
+    private final String uuid;
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
 
     public String getUuid() {
         return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 
     @Override
