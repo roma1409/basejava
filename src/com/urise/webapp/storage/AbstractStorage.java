50a954f15ae8e45a9874ab5899aee86e01ec1760
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-05-05 11:36:49 +0300

    5.1. Refactored.

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -1,19 +1,4 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
 public abstract class AbstractStorage implements Storage {
-    public abstract int size();
-
-    public abstract void save(Resume resume);
-
-    public abstract void delete(String uuid);
-
-    public abstract void clear();
-
-    public abstract Resume[] getAll();
-
-    public abstract void update(Resume resume);
-
-    public abstract Resume get(String uuid);
 }
