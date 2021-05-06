1250e00217837390476c42567eba86f329f3a0c9
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-05-06 19:21:22 +0300

    7. Added 'Generics' usage.

--- src/com/urise/webapp/storage/MapUuidStorage.java
+++ src/com/urise/webapp/storage/MapUuidStorage.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapUuidStorage extends AbstractStorage {
+public class MapUuidStorage extends AbstractStorage<String> {
     private final Map<String, Resume> map = new HashMap<>();
 
     @Override
@@ -16,28 +16,28 @@ public class MapUuidStorage extends AbstractStorage {
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchKey) {
-        map.put(String.valueOf(searchKey), r);
+    protected void doUpdate(Resume r, String searchKey) {
+        map.put(searchKey, r);
     }
 
     @Override
-    protected boolean isExist(Object searchKey) {
-        return map.containsKey(String.valueOf(searchKey));
+    protected boolean isExist(String searchKey) {
+        return map.containsKey(searchKey);
     }
 
     @Override
-    protected void doSave(Resume r, Object searchKey) {
-        map.put(String.valueOf(searchKey), r);
+    protected void doSave(Resume r, String searchKey) {
+        map.put(searchKey, r);
     }
 
     @Override
-    protected Resume doGet(Object searchKey) {
-        return map.get(String.valueOf(searchKey));
+    protected Resume doGet(String searchKey) {
+        return map.get(searchKey);
     }
 
     @Override
-    protected void doDelete(Object searchKey) {
-        map.remove(String.valueOf(searchKey));
+    protected void doDelete(String searchKey) {
+        map.remove(searchKey);
     }
 
     @Override
