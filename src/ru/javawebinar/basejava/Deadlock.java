20814fd6a6e0aa5ba3d160657adada59d326f65b
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-06-01 05:42:32 +0300

    11.HW. Refactoring by remarks.

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -1,25 +1,23 @@
 package ru.javawebinar.basejava;
 
-public class Deadlock {
+public class Deadlock extends Thread {
+    private final Object lock1;
+    private final Object lock2;
+
+    public Deadlock(Object lock1, Object lock2) {
+        this.lock1 = lock1;
+        this.lock2 = lock2;
+    }
+
     public static void main(String[] args) {
         Object lock1 = new Object();
         Object lock2 = new Object();
 
-        Thread thread1 = new MyThread(lock1, lock2);
-        Thread thread2 = new MyThread(lock2, lock1);
+        Thread thread1 = new Deadlock(lock1, lock2);
+        Thread thread2 = new Deadlock(lock2, lock1);
 
         thread1.start();
         thread2.start();
-    }
-}
-
-class MyThread extends Thread {
-    private final Object lock1;
-    private final Object lock2;
-
-    public MyThread(Object lock1, Object lock2) {
-        this.lock1 = lock1;
-        this.lock2 = lock2;
     }
 
     @Override
@@ -33,7 +31,7 @@ class MyThread extends Thread {
 
     private void sleep() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
