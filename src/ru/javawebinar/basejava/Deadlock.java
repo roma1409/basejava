be12047f089f5caf5602d1bdec0468b8f3a77d10
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-06-01 10:04:53 +0300

    11.HW. Refactoring by remarks 2.

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -1,39 +1,31 @@
 package ru.javawebinar.basejava;
 
-public class Deadlock extends Thread {
-    private final Object lock1;
-    private final Object lock2;
-
-    public Deadlock(Object lock1, Object lock2) {
-        this.lock1 = lock1;
-        this.lock2 = lock2;
-    }
+public class Deadlock {
+    private final static String lock1 = "lock1";
+    private final static String lock2 = "lock2";
 
     public static void main(String[] args) {
-        Object lock1 = new Object();
-        Object lock2 = new Object();
-
-        Thread thread1 = new Deadlock(lock1, lock2);
-        Thread thread2 = new Deadlock(lock2, lock1);
-
-        thread1.start();
-        thread2.start();
+        new Thread(() -> ownRun(lock1, lock2)).start();
+        new Thread(() -> ownRun(lock2, lock1)).start();
     }
 
-    @Override
-    public void run() {
+    private static void ownRun(String lock1, String lock2) {
+        System.out.printf("%s tries to capture %s%n", getCurrentThreadName(), lock1);
         synchronized (lock1) {
-            sleep();
+            System.out.printf("%s has captured %s%n", getCurrentThreadName(), lock1);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s tries to capture %s%n", getCurrentThreadName(), lock2);
             synchronized (lock2) {
+                System.out.printf("%s has captured %s%n", getCurrentThreadName(), lock2);
             }
         }
     }
 
-    private void sleep() {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static String getCurrentThreadName() {
+        return Thread.currentThread().getName();
     }
 }
