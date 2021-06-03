cf52d1e79b0460bcfb2e374f717022760734fd75
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-06-03 10:51:11 +0300

    12. HW. Refactoring.

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -2,8 +2,12 @@ package ru.javawebinar.basejava;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
 
 public class MainStreams {
     public static void main(String[] args) {
@@ -15,7 +19,6 @@ public class MainStreams {
     }
 
     /**
-     * реализовать метод через стрим int minValue(int[] values).
      * Метод принимает массив цифр от 1 до 9, надо выбрать уникальные и вернуть минимально возможное число,
      * составленное из этих уникальных цифр. Не использовать преобразование в строку и обратно. Например {1,2,3,3,2,3}
      * вернет 123, а {9,8} вернет 89
@@ -28,16 +31,11 @@ public class MainStreams {
     }
 
     /**
-     * реализовать метод List<Integer> oddOrEven(List<Integer> integers) если сумма всех чисел нечетная - удалить все нечетные,
+     * Если сумма всех чисел нечетная - удалить все нечетные,
      * если четная - удалить все четные. Сложность алгоритма должна быть O(N). Optional - решение в один стрим.
      */
     static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
-        Predicate<Integer> predicate = (sum % 2 == 0) ? (x -> x % 2 == 0) : (x -> x % 2 != 0);
-        return integers.stream()
-                .filter(predicate)
-                .collect(Collectors.toList());
+        Map<Boolean, List<Integer>> map = integers.stream().collect(partitioningBy(x -> x % 2 == 0, toList()));
+        return map.get(map.get(false).size() % 2 != 0);
     }
 }
