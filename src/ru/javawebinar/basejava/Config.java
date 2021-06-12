d22ad63afac90a5de23facbf87473c4bef757eb1
Roman Pogorelov <roman.pogorelov14@gmail.com>
2021-06-12 09:14:20 +0300

    17.HW. Changed properties & config to heroku server.

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -3,13 +3,12 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.storage.SqlStorage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPERTY_FILE = new File(getConfigPath());
+    private static final String PROPERTY_FILE_NAME = getConfigPath();
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -20,12 +19,13 @@ public class Config {
     }
 
     private static String getConfigPath() {
-        String projectPath = "Linux".equals(System.getProperty("os.name")) ? "/home/roman/Desktop" : "C:/Users/roman.pogorelov/Desktop/javaops";
-        return projectPath.concat("/basejava/config/resumes.properties");
+//        String projectPath = "Linux".equals(System.getProperty("os.name")) ? "/home/roman/Desktop" : "C:/Users/roman.pogorelov/Desktop/javaops";
+//        return projectPath.concat("/basejava/config/resumes.properties");
+        return "/resumes.properties";
     }
 
     private Config() {
-        try (InputStream stream = new FileInputStream(PROPERTY_FILE)) {
+        try (InputStream stream = Config.class.getResourceAsStream(PROPERTY_FILE_NAME)) {
             Properties properties = new Properties();
             properties.load(stream);
             String storagePathKey = "Linux".equals(System.getProperty("os.name")) ? "storage.dir.linux" : "storage.dir.win";
@@ -36,7 +36,7 @@ public class Config {
             String password = properties.getProperty("db.password");
             sqlStorage = new SqlStorage(url, user, password);
         } catch (IOException e) {
-            throw new IllegalStateException(String.format("Invalid config file %s", PROPERTY_FILE.getAbsolutePath()));
+            throw new IllegalStateException(String.format("Invalid config file %s", PROPERTY_FILE_NAME));
         }
     }
 
